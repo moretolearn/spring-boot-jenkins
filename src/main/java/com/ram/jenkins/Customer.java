a4afd24f753435a7e8872de92329f3c926b1fc33
moretolearn <rammohanreddy.yanumula@virsham.com>
2020-11-22 00:55:29 +0530

    Added json service

---
+++ src/main/java/com/ram/jenkins/Customer.java
@@ -0,0 +1,16 @@
+package com.ram.jenkins;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+	private Long id;
+	private String name;
+	private Double salary;
+	
+}
--- src/main/java/com/ram/jenkins/JenkinsController.java
+++ src/main/java/com/ram/jenkins/JenkinsController.java
@@ -1,5 +1,8 @@
 package com.ram.jenkins;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,12 @@ public class JenkinsController {
 	@GetMapping("/jenkins")
 	public String getJenkins() {
 		return "welcome to jenkis spring boot project";
+	}
+	
+	@GetMapping("/jenkins-json")
+	public List<Customer> getJenkinsJson() {
+		List<Customer> asList = Arrays.asList(new Customer(1l,"ram",60000d),new Customer(1l,"ram",60000d),new Customer(1l,"ram",60000d),new Customer(1l,"ram",60000d),new Customer(1l,"ram",60000d));
+		return asList;
 	}
 
 }
