8e5a0ea1826fe394f985a22f4c8d312e97a08a41
Y RAM MOHAN REDDY <Y RAM MOHAN REDDY@DESKTOP-K5EE46T>
2023-11-27 07:27:58 +0530

    Code Updated

--- src/main/java/com/ram/jenkins/SpringBootJenkinsApplication.java
+++ src/main/java/com/ram/jenkins/SpringBootJenkinsApplication.java
@@ -2,6 +2,7 @@ package com.ram.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,6 +10,11 @@ public class SpringBootJenkinsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootJenkinsApplication.class);
 	}
 
 }
